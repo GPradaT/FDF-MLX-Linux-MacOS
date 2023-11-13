f8a341e3738b1a55662f6b046acafe117bda0a31
Guillem Prada Torres <gprada-t@cbr4s3.42barcelona.com>
2023-11-13 14:03:09 +0100

    arreglando integracion de printf en fdf

---
+++ lib/ft_printf_guillem/ft_print_pointer.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_pointer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gprada-t <gprada-t@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 00:13:44 by gprada-t          #+#    #+#             */
+/*   Updated: 2023/09/17 18:40:34 by gprada-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_print_pointer(unsigned long ptr, int *len)
+{
+	int		i;
+	char	*base;
+
+	i = 0;
+	base = "0123456789abcdef";
+	ft_print_str("0x", len);
+	if (*len == -1)
+		return ;
+	if (ptr == 0)
+	{
+		ft_put_char('0', len);
+		if (*len == -1)
+			return ;
+		return ;
+	}
+	ft_put_hex(ptr, len);
+	if (*len == -1)
+		return ;
+}
