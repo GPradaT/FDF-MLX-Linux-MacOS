9619572ad8b350006c2c87c7235be2fcfb11d526
Guillem Prada Torres <gpradat@MacBook-Pro-de-Guillem.local>
2024-11-19 08:30:34 +0100

    la proyección isometrica esta aplicada en la matriz de rotación

--- src/matrix.c
+++ src/matrix.c
@@ -13,9 +13,9 @@ void	matrix_rotation(t_matrix *matrix, float alpha_x, float alpha_y, float alpha
 {
 	matrix_identity(matrix);
 
-	alpha_x = alpha_x * M_PI / 180;
-	alpha_y = 90;
-	alpha_z = alpha_z * M_PI / 180;
+	alpha_x = 35.264f * M_PI / 180;
+	alpha_y = 45.0f * M_PI / 180;
+	alpha_z = 0.0f;
 
 	if (alpha_x)
 	{
